0b32b2c7bcc092d0e21ef64035c21e55ebee4381
pamefranco34-lgtm <pamefranco34@gmail.com>
2025-12-14 16:47:38 +0000

    Guardando cambios antes del rebase

---
+++ js/accesibilidad.js
@@ -0,0 +1,15 @@
+function aumentarTexto() {
+    document.body.style.fontSize = "larger";
+}
+
+function disminuirTexto() {
+    document.body.style.fontSize = "smaller";
+}
+
+function toggleContraste() {
+    document.body.classList.toggle("alto-contraste");
+}
+
+function toggleDaltonismo() {
+    document.body.classList.toggle("daltonismo");
+}
